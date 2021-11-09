baf49ffb61c4256a2898130d6870415168352bb1
Pillroy <walkertom1996@gmail.com>
2021-11-09 17:46:48 +0000

    code adjusted to include scrollMode property. currently not triggering - may need import

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-var firstsec = document.getElementById('first-section');
-var secondsec = document.getElementById('second-section');
-var lastScrollTop = 0;
+const firstsec = document.getElementById('first-section');
+const secondsec = document.getElementById('second-section');
+const lastScrollTop = 0;
 window.onscroll = function () {
-  var st = window.pageYOffset || document.documentElement.scrollTop;
+  const st = window.pageYOffset || document.documentElement.scrollTop;
   if (st > lastScrollTop) {
-    secondsec.scrollIntoView({ behavior: 'smooth' });
+    secondsec.scrollIntoView(node, {
+      behavior: 'smooth',
+      scrollMode: 'if-needed',
+    });
   } else {
-    firstsec.scrollIntoView({ behavior: 'smooth' });
+    firstsec.scrollIntoView(node, {
+      behavior: 'smooth',
+      scrollMode: 'if-needed',
+    });
   }
   lastScrollTop = st <= 0 ? 0 : st;
 };
